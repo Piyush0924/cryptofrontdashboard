d4f8b76ad05ebf94f5eb45b684f4190d20f75cdf
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2024-06-28 11:39:43 +0530

    mdx

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [{ enforce: 'pre', ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+
   base: '/cryptofrontdashboard/',
   build: {
     outDir: 'dist',  // Output directory for the build
