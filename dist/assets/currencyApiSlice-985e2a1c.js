a9d1ea3a38d34440a79797120830bef2b04ccb7b
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2024-06-28 21:23:06 +0530

    Remove dist folder from Git tracking and add to .gitignore

--- dist/assets/currencyApiSlice-985e2a1c.js
+++
@@ -1,1 +0,0 @@
-const s=["btc","eth","ltc","bch","bnb","eos","xrp","xlm","link","dot","yfi","usd","aed","ars","aud","bdt","bhd","sgd","thb","try","twd","uah","vef","vnd","zar","xdr","xag","xau","bits","sats"];export{s};
