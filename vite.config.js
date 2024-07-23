4c787275dcb4825466d8ac38cca0496c906ee918
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2024-07-23 16:19:55 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,25 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
  
-  // build: {
-  //   outDir: 'dist',  // Output directory for the build
+  build: {
+    outDir: 'dist',  // Output directory for the build
    
-  //   assetsDir: 'assets',  // Directory for static assets
+    assetsDir: 'assets',  // Directory for static assets
  
-  //   manifest: true, // Generate manifest.json
-  //   sourcemap: false, // Disable source maps for production
-  //   rollupOptions: {
+    manifest: true, // Generate manifest.json
+    sourcemap: false, // Disable source maps for production
+    rollupOptions: {
     
       
-  //     output:
-  //     {
-  //         format: 'es',
-  //         strict: false,
-  //         entryFileNames: "index.jsx",
-  //         dir: 'dist'
-  //     }
-  //  }
-  // },
+      output:
+      {
+          format: 'es',
+          strict: false,
+          entryFileNames: "[name].jsx",
+          dir: 'dist/'
+      }
+   }
+  },
   server: {
     proxy: {
       '/api': {
