25eced4edaa46f8151c97ec859477536a0565fdc
Piyush Pakmode <147019597+Piyush0924@users.noreply.github.com>
2024-11-27 21:43:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
@@ -6,31 +5,11 @@ import { App } from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-// WebSocket Connection
-const connectWebSocket = () => {
-  const socket = new WebSocket('wss://ws-us3.pusher.com/app/7d55ac978a3647512f45?protocol=7&client=js&version=7.0.6&flash=false');
-
-  socket.onopen = () => {
-    console.log("WebSocket connection established");
-  };
-
-  socket.onclose = (event) => {
-    console.error("WebSocket closed:", event.reason);
-    console.log("Retrying connection in 5 seconds...");
-    setTimeout(connectWebSocket, 5000); // Retry connection
-  };
-
-  socket.onerror = (error) => {
-    console.error("WebSocket error:", error);
-  };
-};
-
-// Initialize WebSocket
-connectWebSocket();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
